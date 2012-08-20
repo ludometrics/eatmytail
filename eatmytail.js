9659cb637b3a6bf9a4e154687f8da894929781cb
David Thomson <dwlt@dwlt.net>
2012-08-20 17:02:25 +0100

    High score bits

--- eatmytail.js
+++ eatmytail.js
@@ -1,3 +1,44 @@
+// Cookie functions from: http://www.quirksmode.org/js/cookies.html
+
+function createCookie(name,value,days) {
+	if (days) {
+		var date = new Date();
+		date.setTime(date.getTime()+(days*24*60*60*1000));
+		var expires = "; expires="+date.toGMTString();
+	}
+	else var expires = "";
+	document.cookie = name+"="+value+expires+"; path=/";
+}
+
+function readCookie(name) {
+	var nameEQ = name + "=";
+	var ca = document.cookie.split(';');
+	for(var i=0;i < ca.length;i++) {
+		var c = ca[i];
+		while (c.charAt(0)==' ') c = c.substring(1,c.length);
+		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+	}
+	return null;
+}
+
+function emtSetHighScore(value) {
+	console.log("setHighScore: " + value + "  was " + emtGetHighScore());
+
+	if (value > emtGetHighScore()) {
+		createCookie("emtHighScore", value, 1461);
+		document.getElementById('score-value').innerHTML = value;
+	}
+}
+
+function emtGetHighScore() {
+	var x = readCookie("emtHighScore");
+	if (!x)
+	{
+		x = "0";
+	}
+	return x;
+}
+
 window.onload = (function() {
 	var WIDTH = 256,
 			HEIGHT  = 256,
@@ -128,6 +169,7 @@ window.onload = (function() {
 											.collision().onHit("snake", function() {
 												if (segments.length > 1 && !scored) {
 													scorePanel = Crafty.e("2D, DOM, Text").attr({x: 16, y: 16, width: 256}).textFont({size: '14px', family: "Futura, Helvetica, sans-serif"}).text("YOU SCORE " + (this.segment * this.segment));
+													emtSetHighScore(this.segment * this.segment);
 													scored = true;
 												}
 											});
